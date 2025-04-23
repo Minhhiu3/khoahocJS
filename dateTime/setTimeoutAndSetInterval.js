b8f3bf05d4b558bedadc17ad73fb5aa90d762813
Minhhiu3 <vmink2004@gmail.com>
2025-04-23 22:50:11 +0700

    lab js

--- dateTime/setTimeoutAndSetInterval.js
+++ dateTime/setTimeoutAndSetInterval.js
@@ -32,4 +32,14 @@ function clock() {
         document.getElementById('hour').innerText = hour;
     }, 1000);
 }
+
+function clockRealTime() {
+    const now = new Date();
+    const h = now.getHours().toString().padStart(2, '0');
+    const m = now.getMinutes().toString().padStart(2, '0');
+    const s = now.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById('clock').innerText = `${h}:${m}:${s}`;
+}
 clock()
+setInterval(clockRealTime(), 1000)
